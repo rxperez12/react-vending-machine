4ed82b921f3dd8267b66d1ed78314b1769d1b54a
Ricardo Perez <rxperez12@gmail.com>
2024-06-03 12:02:34 -0700

    added router with route for apple -WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
-import { useState } from 'react';
-import logo from './logo.svg'
-import './App.css';
-
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Apple from "./Apple.jsx";
+import VendingMachine from "./VendingMachine.jsx";
+import "./App.css";
 
 /** Component for entire page.
  *
  * Props: none
  * State: none
  *
-*/
+ */
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function incrCount() {
-    setCount(count => count + 1);
-  }
-
   return (
     <div className="App">
-      <main>
-        <img src={logo} className="App-logo" alt="Rithm" />
-        <h1>Rithm React Starter</h1>
-        <p>
-          <button
-              className="btn btn-primary"
-              onClick={incrCount}>
-            Clicked: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>src/App.jsx</code>
-        </p>
-      </main>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<VendingMachine />}
+          />
+          <Route
+            path="/apple"
+            element={<Apple />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-};
+}
 
 export default App;
