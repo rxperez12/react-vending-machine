fdd0c8df26793500bd9cb286ce11b5eb2295e38c
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-03 15:31:21 -0400

    Updated Link components

--- src/VendingMachine.jsx
+++ src/VendingMachine.jsx
@@ -1,3 +1,17 @@
-function VendingMachine() {}
+import { Link } from "react-router-dom";
+import "./VendingMachine.css";
+
+function VendingMachine() {
+  return (
+    <div className="VendingMachine" >
+      <ul>
+        <li><Link to="/">home</Link></li>
+        <li><Link to="/apple">apple</Link></li>
+        <li><Link to="/ice-cream-sandwich">ice cream sandwich</Link></li>
+        <li><Link to="/hot-cheetos">hot cheetos</Link></li>
+      </ul>
+    </div>
+  );
+}
 
 export default VendingMachine;
